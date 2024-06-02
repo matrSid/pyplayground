58500d5da9ebbb0049ac284d7de4417f2d8b00ac
matrSid <165424989+matrSid@users.noreply.github.com>
2024-06-03 01:28:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,16 +32,18 @@ const PythonPlayground = () => {
     });
 
     // Custom time module
-    const timeModule = {
+    const timeModule = new Sk.builtin.module({
+      __name__: new Sk.builtin.str("time"),
       sleep: new Sk.builtin.func(function (seconds) {
         return new Promise(resolve => setTimeout(resolve, Sk.ffi.remapToJs(seconds) * 1000));
-      })
-    };
+      }),
+    });
 
     // Custom random module
-    const randomModule = {
+    const randomModule = new Sk.builtin.module({
+      __name__: new Sk.builtin.str("random"),
       random: new Sk.builtin.func(function () {
-        return Math.random();
+        return Sk.ffi.remapToPy(Math.random());
       }),
       randint: new Sk.builtin.func(function (a, b) {
         a = Sk.ffi.remapToJs(a);
@@ -52,7 +54,7 @@ const PythonPlayground = () => {
         const index = Math.floor(Math.random() * Sk.ffi.remapToJs(seq).length);
         return seq.mp$subscript(index);
       })
-    };
+    });
 
     Sk.sysmodules.mp$ass_subscript(new Sk.builtin.str('time'), timeModule);
     Sk.sysmodules.mp$ass_subscript(new Sk.builtin.str('random'), randomModule);
